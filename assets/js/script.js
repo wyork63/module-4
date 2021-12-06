d9a738d020ba3345b1f6562fea7a075a90da8fe8
wyork <w.york63@gmail.com>
2021-12-06 15:51:09 -0500

    updated taskhandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,23 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event) {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("Input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // create list item 
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info
+    var taskInfoEl = document.createElement("div");
+    // git it a name
+    taskInfoEl.className = "task-info";
+    // add html content 
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 
 }
