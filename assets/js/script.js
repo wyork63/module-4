b4acb33bfcf9144cfa3ddd63b272fedd7e690a46
wyork <w.york63@gmail.com>
2021-12-06 16:28:04 -0500

    enhanced  usability

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,13 @@ var taskFormHandler = function (event) {
     var taskNameInput = document.querySelector("Input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  //check if input values are empty string
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false; 
+  }
+  formEl.reset();
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
